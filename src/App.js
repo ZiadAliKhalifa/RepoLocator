a2c0decce85186d316edbc52f169548880cad3ba
ziadalikhalifa <ziadalikhalifa@gmail.com>
2019-11-23 12:39:51 +0200

    Added the search bar into the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
+
+import SearchBar from "./common/SearchBar";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <Grid container justify="center">
+        <Grid item sm={6}>
+          <SearchBar />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default App;
