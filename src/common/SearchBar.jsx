f09ebb8cffe414630c31b5a141564baa82329178
ZiadAliK <ziad.ali@itworx.com>
2019-11-23 13:38:43 +0200

    Implemented function to run when user stopped typing for a while

--- src/common/SearchBar.jsx
+++ src/common/SearchBar.jsx
@@ -5,14 +5,39 @@ import SearchIcon from "@material-ui/icons/Search";
 const SearchBar = () => {
   const [searchPhrase, setSearchPhrase] = useState("");
 
+  const typingStoppedInterval = 1500; //App will wait for a second and a half after user stopped typing to search
+  let timer = null;
+
+  const handleKeyPress = () => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (searchPhrase) {
+      timer = setTimeout(doneTyping, typingStoppedInterval);
+    }
+  };
+
+  const handleTextChange = () => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+  };
+
+  const doneTyping = () => {
+    //Trigger API call
+  };
+
   return (
     <>
       <Input
         id="searchBar"
         value={searchPhrase}
+        style={{ fontSize: 40 }}
         onChange={e => {
           setSearchPhrase(e.target.value);
+          handleTextChange();
         }}
+        onKeyUp={handleKeyPress}
         endAdornment={
           <InputAdornment position="end">
             <SearchIcon />
