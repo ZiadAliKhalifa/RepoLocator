5df3190780b068733ebb4d6d0f8e6177908c5bc0
ZiadAliK <ziad.ali@itworx.com>
2019-11-23 13:04:26 +0200

    Added input adorement

--- src/common/SearchBar.jsx
+++ src/common/SearchBar.jsx
@@ -1,25 +1,6 @@
 import React, { useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import Input from "@material-ui/core/Input";
-import InputAdornment from "@material-ui/core/InputAdornment";
+import { Input, InputAdornment } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap"
-  },
-  margin: {
-    margin: theme.spacing(1)
-  },
-  withoutLabel: {
-    marginTop: theme.spacing(3)
-  },
-  textField: {
-    width: 200
-  }
-}));
 
 const SearchBar = () => {
   const [searchPhrase, setSearchPhrase] = useState("");
@@ -27,7 +8,7 @@ const SearchBar = () => {
   return (
     <>
       <Input
-        id="search"
+        id="searchBar"
         value={searchPhrase}
         onChange={e => {
           setSearchPhrase(e.target.value);
