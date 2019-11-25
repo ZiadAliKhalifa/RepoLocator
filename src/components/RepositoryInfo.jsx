e29a75258ba5b8e1a29a7f8cf7096f79a268db96
ZiadAliK <ziad.ali@itworx.com>
2019-11-25 23:08:11 +0200

    Added loader to the repo info page

---
+++ src/components/RepositoryInfo.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+
+import CircularUnderLoad from "./LoadingSpinner";
+import { loadRepositoryInfo } from "../redux/actions/repositoryActions";
+
+export default function RepositoryInfo() {
+  // useEffect(() => {
+  //   loadRepositoryInfo("gionkunz", "chartist-js");
+  // }, []);
+
+  return (
+    <Grid container justify="center">
+      <CircularUnderLoad />
+    </Grid>
+  );
+}
